0a5f18aaaf7ecc29c8fb809bec2aeb5fe371ff30
dgogi17@freeuni.edu.ge <dgogi17@freeuni.edu.ge>
2024-01-22 21:19:48 +0400

    added laser collision and auto destruction. added method invocation of other scenes

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -7,6 +7,13 @@ extends CharacterBody2D
 ## Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _process(_delta):
+	var direction = Vector2.RIGHT
+	velocity = direction *100
+	move_and_slide()
+	
+func hit():
+	print("dmg")
 
 func _physics_process(_delta):
 	#var direction = Vector2.RIGHT
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -6,3 +6,15 @@ var direction:Vector2= Vector2.UP
 func _process(delta):
 	position += direction * speed * delta
 	
+func _ready():
+	$SelfDestruction.start()
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	queue_free()
+
+
+
+func _on_timer_timeout():
+	queue_free() # Replace with function body.
