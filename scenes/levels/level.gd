bfe83a3aa83137abb1ac632954e6e643a008dac3
dgogi17@freeuni.edu.ge <dgogi17@freeuni.edu.ge>
2024-01-22 22:30:59 +0400

    created parent level with general behaviours. Implemented Inheritence design to create outside level based on the parent level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,17 +1,10 @@
 extends Node2D
-
+class_name ParentLevel
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 var drone_scene: PackedScene = preload("res://scenes/enemies/drone.tscn")
 const default_player_zoom = Vector2(0.5,0.5)
 const house_player_zoom = Vector2(1,1)
-
-func _on_gate_player_entered_gate(body):
-	print("player entered gate")
-	print(body)
-
-func _ready():
-	$DroneSpawnTimer.start()
 
 func _on_player_laser(laser_position, _laser_direction):
 	var laser = laser_scene.instantiate()
@@ -25,18 +18,3 @@ func _on_player_grenade(grenade_position, grenade_direction):
 	grenade.position = grenade_position
 	grenade.linear_velocity = grenade.speed * grenade_direction
 	$Projectiles.add_child(grenade)
-
-func _on_house_player_entered():
-	set_player_zoom(house_player_zoom)
-
-func _on_house_player_exit():
-	set_player_zoom(default_player_zoom)
-	
-func set_player_zoom(zoom):
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D,"zoom",zoom,1)
-
-func _on_drone_spawn_timer_timeout():
-	var drone = drone_scene.instantiate() as CharacterBody2D
-	drone.position = Vector2(153,828)
-	$".".add_child(drone)
---
+++ scenes/levels/outside.gd
@@ -0,0 +1,29 @@
+extends ParentLevel
+
+func _on_gate_player_entered_gate(body):
+	print("player entered gate")
+	print(body)
+	
+# Called when the node enters the scene tree for the first time.	
+func _ready():
+	$DroneSpawnTimer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+func _on_house_player_entered():
+	set_player_zoom(house_player_zoom)
+
+func _on_house_player_exit():
+	set_player_zoom(default_player_zoom)
+	
+func set_player_zoom(zoom):
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D,"zoom",zoom,1)
+
+func _on_drone_spawn_timer_timeout():
+	var drone = drone_scene.instantiate() as CharacterBody2D
+	drone.position = Vector2(153,828)
+	$".".add_child(drone)
